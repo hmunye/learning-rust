4a9f21c8fcb98d8d1dcb71f38fbfe0fa1b37f81f
Huseen <136820327+hmunye@users.noreply.github.com>
2024-07-11 08:52:20 -0400

    chapter 1

---
+++ README.md
@@ -0,0 +1,18 @@
+# Learning Rust
+
+## Commands Used
+
+```shell
+rustup <update> <doc>
+```
+> A command line tool for managing Rust versions and associated tools
+
+```shell
+rustc <file_name>
+```
+> The Rust compiler
+
+```shell
+cargo <new> <build> <run> <check> 
+```
+> Rust's build system and package manager
---
+++ hello_cargo/src/main.rs
@@ -0,0 +1,10 @@
+// The main function is always the first code to run in a Rust program
+//
+// Uses C like syntax with curly braces wrapping the function body, parentheses to pass in
+// parameters, and semicolons (;) to end expressions
+fn main() {
+    // This line calls a Rust macro, signified by the '!'
+    //
+    // Here, a string literal is passed as an argument to 'println!' and is printed to stdout
+    println!("Hello, world!");
+}
